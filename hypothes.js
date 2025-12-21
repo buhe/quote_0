8a875eb0b55341090d4de874ddb54bf285b45943
buhe <bugu1986@126.com>
2025-12-21 21:21:34 +0800

    Add Hypothes.is API handler function
    Introduces a new handler that fetches recent annotations from the Hypothes.is API, selects one based on the current hour, and posts its details to an external API endpoint. Handles errors and returns the selected annotation's text.

---
+++ hypothes.js
@@ -0,0 +1,53 @@
+export const handler = async (event) => {
+    try {
+        const nowStr = new Date().toISOString()          // 2025-12-20T19:46:09.334Z
+            .replace('Z', '+00:00')           // 2025-12-20T19:46:09.334+00:00
+            .replace(/(\.\d{3})\d*/, '$100');  // 不足 6 位补 0 → 2025-12-20T19:46:09.334000+00:00
+
+        const url = `https://hypothes.is/api/search?group=3gxjr5RN&sort=created&search_after=${nowStr}&limit=12`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer 6879-bJdzXWQSCno-ONUfjttuoz0ytLffeK3-sSigHJKUfmg'
+            }
+        };
+
+        const response = await fetch(url, options);
+        const data = await response.json();
+        const rows = data.rows;
+        //   console.log(rows.length);
+        const hour = new Date().getHours();
+
+        const nowItem = rows[hour % rows.length];
+        const title = nowItem.document.title;
+        const exact = nowItem.target.selector[2].exact;
+        const link = nowItem.target.source;
+
+        const url2 = 'https://dot.mindreset.tech/api/authV2/open/device/48F6EE55B498/text';
+        const options2 = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer dot_app_rNdgyLmXPksJdkFwBrjPqguonlTXIZSJgHRTDjvhjgIagfmlcONIsXpTAxkYESwj'
+            },
+            body: `{"refreshNow":true,"title":"${title}","message":"${exact}","link":"${link}"}`
+        };
+
+        const response2 = await fetch(url2, options2);
+        const data2 = await response2.text();
+        // console.log(data);
+        const response3 = {
+            statusCode: 200,
+            body: JSON.stringify(exact),
+        };
+        return response3;
+    } catch (e) {
+        console.error(e);
+        const response2 = {
+            statusCode: 500,
+            body: JSON.stringify(e.message),
+        };
+        return response2;
+    }
+}
