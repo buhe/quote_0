845b1c859ac99795c695edd15078403a815e2517
buhe <bugu1986@126.com>
2025-12-29 17:32:55 +0800

    Add signature fields to API payloads
    Added 'signature' fields to the payloads sent in hypothes.js and readhub_daily.js to include creation date and site name, respectively. Updated the request body structure to support these new fields. Removed large commented Rust code from crypto.js for clarity.

--- hypothes.js
+++ hypothes.js
@@ -24,6 +24,7 @@ export const handler = async (event) => {
         // console.log(`title: ${title}`)
         const exact = nowItem.target[0].selector[2].exact;
         const link = nowItem.links.html;
+        const signature = nowItem.created.substring(0, 10);
 
         const url2 = 'https://dot.mindreset.tech/api/authV2/open/device/48F6EE55B498/text';
         const options2 = {
@@ -32,7 +33,7 @@ export const handler = async (event) => {
                 'Content-Type': 'application/json',
                 Authorization: 'Bearer dot_app_rNdgyLmXPksJdkFwBrjPqguonlTXIZSJgHRTDjvhjgIagfmlcONIsXpTAxkYESwj'
             },
-            body: `{"refreshNow":false,"icon":"iVBORw0KGgoAAAANSUhEUgAAAFAAAABQCAMAAAC5zwKfAAAAtFBMVEXSKDj////PAAD+/f3dhIjSMj/TNkLSLjzOAAD89/fRACPVU1vSKTjmrK7TNEHSLz3SJTb89vbTOUX03d7TPkjVTVbRByfMAADmrrD89PX78fLehYnVRE7SIzTSHDHRGC7++vvx0tPptLb57O324OHYZWvWVV3QABH9+Pjvy83txcfVSVLRESvswcLkpKbjn6Lil5rfjpHcen/bc3jaa3HXXGP46Onz19nnr7HWT1jRBCbQABm6Pzr0AAACPUlEQVRYw+2W6W6jMBRG8XXADiGGELckhC37viddZt7/vcaQhnSqUeOANX/KsZAsCx19tnWtq1VUVFRU/Fcci9zBch7xERaZd4gYkY9nx+BOGt8ycSG2ZUPaK3fh1+/gL9yVLeezCPSRBH0gllzA6QkjfE8nfjlN5SLq0ENS9ECXFNY+QqR8I6w9IsxRKVxvOs3BTKHw6IEXdxUKe1PuTFoKhTUwnp9+oLCLcIbyhFhdwtai+bZ4EUoVwl9LPGpQSmG1qyOsQPh73I+BM6YT75AgXFrI4mWwCiyXaSTwBgqEuhVrJp3SqR66ME5QeaGTbno0P4FOwuhFgZA8P/li5r+bBoERwmWFtkEHKMEYbamrQ1uB0E5jifFGDSVCFrJhViXNmxALSlTKspsJO7kwk+GStXwTipH058Mytfw1oX+kEM0RLp0wu2XBzuMcoiHCBR+H25btLGG9EdoWh3khYQ/c+P0q5EYmTM4mtzntF0tIQwguZ9j0wpi209naMyPvmBQS7t3zcnxJOLDOE/dSeuvxZOcXu5SZaANnKCOpCzDKwDPxKextcIHSU97OHRC+33BidJAUkogNEb6Ry78sDllENCkY1Lr/Ora/U3drwDRJdGhsm1c6m/b1iWlvOvnytiE2LItjmDQHvMC/vFt+4AHNMQ1Hk4cZOYzT3setUv55XXsMK0dz6SC91AF1P6+WwCbwitArEFtTgzjSoNUK0kNTgxVyut9THlqaKqPDAJgjfOqMui58FRUVFYX5Az4bNPF95dvAAAAAAElFTkSuQmCC","taskKey":"gFQrtWcXHIOk","title":"${title}","message":"${exact}","link":"${link}"}`
+            body: `{"refreshNow":false,"signature":"Created at ${signature}","title":"${title}","message":"${exact}","link":"${link}"}`
         };
 
         const response2 = await fetch(url2, options2);
